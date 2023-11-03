bfe91a20ca8e87fb39be80e1a11ddaf9380da407
GHassemi.Ehsan <ghassemi.ehsan.official@gmail.com>
2023-11-03 20:15:09 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ import { connect } from 'cloudflare:sockets';
 // t.me/P_tech2024 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '2cd61524-9843-43ab-8a0a-df4a744bb859';
+let userID = '8e941a75-1001-42b3-8ed0-4050c7fa108d';
 
 const proxyIPs = ['mtn.ircf.space', 'mkh.ircf.space', 'mci.ircf.space', 'rtl.ircf.space'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
